b8cd5c93cbb02e8e582bada91bd0bbc8afd78273
Alex <33004994+alexhooley@users.noreply.github.com>
2019-07-12 13:14:24 +0100

    example added

---
+++ example.js
@@ -0,0 +1,190 @@
+<script type="application/ld+json">
+{
+  "@type": "ItemList",
+  "@context": "http://schema.org",
+  "numberOfItems": 4,
+  "itemListElement": [
+    {
+      "@type": "ListItem",
+      "position": 1,
+      "item": {
+        "@type": "Product",
+        "name": "Widget 1",
+        "url": "https://example.com/my-widget-review/#widget1",
+        "image": "https://example.com/images/widget1.jpg",
+        "description": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.",
+        "offers": [
+          {
+            "@type": "Offer",
+            "name": "Widget 1",
+            "price": "99.99",
+            "priceCurrency": "GBP",
+            "url": "https://the-widget-market.example/widgets/widget1",
+            "offeredBy": {
+              "@type": "Organization",
+              "name": "The Widget Market"
+            }
+          },
+          {
+            "@type": "Offer",
+            "name": "Widget 1",
+            "price": "69.99",
+            "priceCurrency": "GBP",
+            "url": "https://widgets-r-us.example/widgets/widget1",
+            "offeredBy": { "@type": "Organization", "name": "Widgets R Us" }
+          },
+          {
+            "@type": "Offer",
+            "name": "Widget 1",
+            "price": "86.00",
+            "priceCurrency": "GBP",
+            "url": "https://the-widget-shop.example/widgets/widget1",
+            "offeredBy": {
+              "@type": "Organization",
+              "name": "The Widget Shop"
+            }
+          }
+        ]
+      }
+    },
+    {
+      "@type": "ListItem",
+      "position": 2,
+      "item": {
+        "@type": "Product",
+        "name": "Widget 2",
+        "url": "https://example.com/my-widget-review/#widget2",
+        "image": "https://example.com/images/widget2.jpg",
+        "description": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.",
+        "offers": [
+          {
+            "@type": "Offer",
+            "name": "Widget 2",
+            "price": "99.99",
+            "priceCurrency": "GBP",
+            "url": "https://the-widget-market.example/widgets/widget2",
+            "offeredBy": {
+              "@type": "Organization",
+              "name": "The Widget Market"
+            }
+          },
+          {
+            "@type": "Offer",
+            "name": "Widget 2",
+            "price": "69.99",
+            "priceCurrency": "GBP",
+            "url": "https://widgets-r-us.example/widgets/widget2",
+            "offeredBy": {
+              "@type": "Organization",
+              "name": "Widgets R Us"
+            }
+          },
+          {
+            "@type": "Offer",
+            "name": "Widget 2",
+            "price": "86.00",
+            "priceCurrency": "GBP",
+            "url": "https://the-widget-shop.example/widgets/widget2",
+            "offeredBy": {
+              "@type": "Organization",
+              "name": "The Widget Shop"
+            }
+          }
+        ]
+      }
+    },
+    {
+      "@type": "ListItem",
+      "position": 3,
+      "item": {
+        "@type": "Product",
+        "name": "Widget 3",
+        "url": "https://example.com/my-widget-review/#widget3",
+        "image": "https://example.com/images/widget3.jpg",
+        "description": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.",
+        "offers": [
+          {
+            "@type": "Offer",
+            "name": "Widget 3",
+            "price": "99.99",
+            "priceCurrency": "GBP",
+            "url": "https://the-widget-market.example/widgets/widget3",
+            "offeredBy": {
+              "@type": "Organization",
+              "name": "The Widget Market"
+            }
+          },
+          {
+            "@type": "Offer",
+            "name": "Widget 3",
+            "price": "69.99",
+            "priceCurrency": "GBP",
+            "url": "https://widgets-r-us.example/widgets/widget3",
+            "offeredBy": {
+              "@type": "Organization",
+              "name": "Widgets R Us"
+            }
+          },
+          {
+            "@type": "Offer",
+            "name": "Widget 3",
+            "price": "86.00",
+            "priceCurrency": "GBP",
+            "url": "https://the-widget-shop.example/widgets/widget3",
+            "offeredBy": {
+              "@type": "Organization",
+              "name": "The Widget Shop"
+            }
+          }
+        ]
+      }
+    },
+    {
+      "@type": "ListItem",
+      "position": 4,
+      "item": {
+        "@type": "Product",
+        "name": "Widget 4",
+        "url": "https://example.com/my-widget-review/#widget4",
+        "image": "https://example.com/images/widget4.jpg",
+        "description": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.",
+        "offers": [
+          {
+            "@type": "Offer",
+            "name": "Widget 4",
+            "price": "99.99",
+            "priceCurrency": "GBP",
+            "url": "https://the-widget-market.example/widgets/widget4",
+            "offeredBy": {
+              "@type": "Organization",
+              "name": "The Widget Market"
+            }
+          },
+          {
+            "@type": "Offer",
+            "name": "Widget 4",
+            "price": "69.99",
+            "priceCurrency": "GBP",
+            "url": "https://widgets-r-us.example/widgets/widget4",
+            "offeredBy": {
+              "@type": "Organization",
+              "name": "Widgets R Us"
+            }
+          },
+          {
+            "@type": "Offer",
+            "name": "Widget 4",
+            "price": "86.00",
+            "priceCurrency": "GBP",
+            "url": "https://the-widget-shop.example/widgets/widget4",
+            "offeredBy": {
+              "@type": "Organization",
+              "name": "The Widget Shop"
+            }
+          }
+        ]
+      }
+    }
+  ]
+}
+</script>
